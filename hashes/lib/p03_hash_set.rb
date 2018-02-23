0ef318fce6c60c1c44a19fbcd53366116e14c2cd
Hsuanchen Wan <WalterHWan@gmail.com>
2018-02-23 15:15:28 -0800

    HashSet Initialize

--- hashes/lib/p03_hash_set.rb
+++ hashes/lib/p03_hash_set.rb
@@ -1,7 +1,7 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_accessor :count
 
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { Array.new }
@@ -9,18 +9,27 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      resize! if count == num_buckets
+      self[key] << key
+      self.count += 1
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    self[key].delete(key)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket = (num.hash % @store.length)
+    @store[bucket]
   end
 
   def num_buckets
@@ -28,5 +37,12 @@ class HashSet
   end
 
   def resize!
+    arr2 = Array.new(num_buckets * 2) { [] }
+    @store.each do |bucket|
+      bucket.each do |el|
+        arr2[el.hash % arr2.length] << el
+      end
+    end
+    @store = arr2
   end
 end
